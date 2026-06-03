0da1f168adc907f506ca28370e0b780d17582117
Carnage098 <polairebleu7@gmail.com>
2026-06-03 16:30:24 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,71 @@
+import aiosqlite
 
+DB_NAME = "database.db"
+
+async def init_db():
+
+    async with aiosqlite.connect(DB_NAME) as db:
+
+        # -------------------
+        # TOURNOIS
+        # -------------------
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS tournaments(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            active INTEGER DEFAULT 1
+        )
+        """)
+
+        # -------------------
+        # TEAMS
+        # -------------------
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS teams(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE,
+            points INTEGER DEFAULT 0
+        )
+        """)
+
+        # -------------------
+        # JOUEURS
+        # -------------------
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS players(
+            discord_id TEXT PRIMARY KEY,
+            username TEXT,
+            deck TEXT,
+            team_name TEXT
+        )
+        """)
+
+        # -------------------
+        # MATCHS
+        # -------------------
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS matches(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            player_id TEXT,
+            player_name TEXT,
+
+            opponent_id TEXT,
+            opponent_name TEXT,
+
+            player_team TEXT,
+            opponent_team TEXT,
+
+            score TEXT,
+
+            player_deck TEXT,
+            opponent_deck TEXT,
+
+            status TEXT DEFAULT 'pending'
+        )
+        """)
+
+        await db.commit()
