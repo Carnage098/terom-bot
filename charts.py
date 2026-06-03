f60a53a218d97929ddea0b975f7cf8803aba09e2
Carnage098 <polairebleu7@gmail.com>
2026-06-03 17:09:13 +0200

    Update charts.py

--- charts.py
+++ charts.py
@@ -1,1 +1,22 @@
+import matplotlib.pyplot as plt
 
+def create_deck_graph(deck_data, filename):
+
+    labels = []
+    values = []
+
+    for deck, count in deck_data:
+        labels.append(deck)
+        values.append(count)
+
+    plt.figure(figsize=(8,5))
+    plt.bar(labels, values)
+
+    plt.title("Decks les plus joués")
+    plt.ylabel("Nombre d'utilisations")
+
+    plt.tight_layout()
+
+    plt.savefig(filename)
+
+    plt.close()
