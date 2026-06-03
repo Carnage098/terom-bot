e7efc80260f369cf2c2aa336e98d1fc2272bbae4
Carnage098 <polairebleu7@gmail.com>
2026-06-03 16:29:53 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,1 @@
-STAFF_ROLES = [
-    "Admin",
-    "🛑Modo"
-]
 
-def is_staff(member):
-
-    if member.guild_permissions.administrator:
-        return True
-
-    for role in member.roles:
-
-        if role.name in STAFF_ROLES:
-            return True
-
-    return False
